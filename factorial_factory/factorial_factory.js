6986effe2c777be613c734299ee89a17f79cdc34
Nic Mitchell <nic@nicmitchell.com>
2015-02-28 11:39:00 -0800

    factorial_factory recursive solution

--- factorial_factory/factorial_factory.js
+++ factorial_factory/factorial_factory.js
@@ -1,17 +1,5 @@
 function factorial (n) {
-  if (n === 0) return 1;
   if (n < 0) return null;
-
-  var fac = 1;
-  var total = 1;
-
-  while(fac < n){
-    total *= ++fac;
-    console.log('fac', fac);
-    console.log('total', total);
-  }
-  
-  return total;
+  if (n === 0) return 1;
+  return n * factorial(n - 1);
 }
-
-console.log(factorial(5));
